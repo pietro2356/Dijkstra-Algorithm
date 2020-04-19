19c4cb895f49817b07a1f4f17b4cc11fabcc7697
pietro.rocchio <pietro.rocchio@marconirovereto.it>
2020-04-19 17:51:13 +0200

    Implementata la tabella dei costi.

--- python-code/DijkstraPY.py
+++ python-code/DijkstraPY.py
@@ -1,3 +1,5 @@
+import math
+
 # Implementazione del grafo
 grafo = {}
 grafo["start"] = {}
@@ -12,3 +14,9 @@ grafo["b"]["a"] = 3
 grafo["b"]["end"] = 5
 
 grafo["end"] = {}
+
+# Tabella costi
+costo_nodi = {}
+costo_nodi["a"] = 6
+costo_nodi["b"] = 2
+costo_nodi["end"] = math.inf

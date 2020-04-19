1b00d3773ef67075ffb25452fdc64a8e42ca8de0
pietro.rocchio <pietro.rocchio@marconirovereto.it>
2020-04-19 18:37:31 +0200

    Fine implementazione algoritmo. si procede ad eseguire dei test.

--- python-code/DijkstraPY.py
+++ python-code/DijkstraPY.py
@@ -29,13 +29,13 @@ parents["end"] = None
 
 processati = []
 
+
 def nodo_con_costo_minore(costo_nddi):
     costo_minimo = math.inf         # Costo minimo del nodo attuale. [Ovviamente non è ancora stato calcoalto]
     nodo_con_costo_minimo = None    # Nodo con il costo minimo. Verrà preso in considerazione per il percorso.
 
     for n in costo_nodi:
         costo_nodo = costo_nodi[n]  # costo del nodo attuale.
-
         if (costo_nodo < costo_minimo) and (n not in processati):
             costo_minimo = costo_nodo   # Assegno come costo minimo il costo el nodo appena analizzato.
             nodo_con_costo_minimo = n   # Assegno Il nodo con l'effettivo costo minimo alla variabile.
@@ -48,13 +48,12 @@ nodo = nodo_con_costo_minore(costo_nodi)    # La funzione restituisce il nodo co
 while nodo is not None:
     costo_nodo = costo_nodi[nodo]   # costo nodo corrente.
     vicini = grafo[nodo]    # ricavo i nodi vicini
-
     for n in vicini.keys():
         nuovo_costo_nodo = costo_nodo + vicini[n]   # costo per arrivare al nodo che sto esaminando.
-                                                    # Partendo da start
         if costo_nodi[n] > nuovo_costo_nodo:
             costo_nodi[n] = nuovo_costo_nodo
             parents[n] = nodo
-
     processati.append(nodo)     # inserisco il nodo appena processato in questo array in modo da non creare loop.
     nodo = nodo_con_costo_minore(costo_nodi)
+
+print(parents)

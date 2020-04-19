3b87b2d508202a91f4725fcbc268827fc3680f16
pietro.rocchio <pietro.rocchio@marconirovereto.it>
2020-04-20 01:00:41 +0200

    Modificati i nomi delle variabili.

--- python-code/DijkstraPY.py
+++ python-code/DijkstraPY.py
@@ -1,4 +1,3 @@
-import sys
 import math
 
 # Implementazione del grafo:
@@ -28,13 +27,13 @@ grafo["e"]["end"] = 1
 grafo["end"] = {}
 
 # Tabella costi:
-costo_nodi = {}
-costo_nodi["a"] = 2
-costo_nodi["b"] = math.inf
-costo_nodi["c"] = math.inf
-costo_nodi["d"] = 8
-costo_nodi["e"] = math.inf
-costo_nodi["end"] = math.inf
+costoNodi = {}
+costoNodi["a"] = 2
+costoNodi["b"] = math.inf
+costoNodi["c"] = math.inf
+costoNodi["d"] = 8
+costoNodi["e"] = math.inf
+costoNodi["end"] = math.inf
 
 # Tabella parents:
 parents = {}
@@ -49,33 +48,33 @@ processati = []
 
 
 def nodo_con_costo_minore(costo_nddi):
-    costo_minimo = math.inf           # Costo minimo del nodo attuale. [Ovviamente non è ancora stato calcoalto]
-    nodo_con_costo_minimo = None    # Nodo con il costo minimo. Verrà preso in considerazione per il percorso.
+    costoMinimo = math.inf  # Costo minimo del nodo attuale. [Ovviamente non è ancora stato calcoalto]
+    nodoConCostoMinimo = None  # Nodo con il costo minimo. Verrà preso in considerazione per il percorso.
 
-    for n in costo_nodi:
-        costo_nodo = costo_nodi[n]  # costo del nodo attuale.
-        if (costo_nodo < costo_minimo) and (n not in processati):
-            costo_minimo = costo_nodo   # Assegno come costo minimo il costo el nodo appena analizzato.
-            nodo_con_costo_minimo = n   # Assegno Il nodo con l'effettivo costo minimo alla variabile.
+    for n in costoNodi:
+        costoSingoloNodo = costoNodi[n]  # costo del nodo attuale.
+        if (costoSingoloNodo < costoMinimo) and (n not in processati):
+            costoMinimo = costoSingoloNodo  # Assegno come costo minimo il costo el nodo appena analizzato.
+            nodoConCostoMinimo = n  # Assegno Il nodo con l'effettivo costo minimo alla variabile.
 
-    return nodo_con_costo_minimo    # Restituisco il nodo effettivo col costo minimo.
+    return nodoConCostoMinimo  # Restituisco il nodo effettivo col costo minimo.
 
 
-nodo = nodo_con_costo_minore(costo_nodi)    # La funzione restituisce il nodo con costo minore.
+nodo = nodo_con_costo_minore(costoNodi)  # La funzione restituisce il nodo con costo minore.
 
 while nodo is not None:
-    costo_nodo = costo_nodi[nodo]   # costo nodo corrente.
-    vicini = grafo[nodo]    # ricavo i nodi vicini
+    costoNodo = costoNodi[nodo]  # costo nodo corrente.
+    vicini = grafo[nodo]  # ricavo i nodi vicini
     for n in vicini.keys():
-        nuovo_costo_nodo = costo_nodo + vicini[n]   # costo per arrivare al nodo che sto esaminando.
-        if costo_nodi[n] > nuovo_costo_nodo:
-            costo_nodi[n] = nuovo_costo_nodo
+        nuovoCostoNodo = costoNodo + vicini[n]  # costo per arrivare al nodo che sto esaminando.
+        if costoNodi[n] > nuovoCostoNodo:
+            costoNodi[n] = nuovoCostoNodo
             parents[n] = nodo
-    processati.append(nodo)     # inserisco il nodo appena processato in questo array in modo da non creare loop.
-    nodo = nodo_con_costo_minore(costo_nodi)
+    processati.append(nodo)  # inserisco il nodo appena processato in questo array in modo da non creare loop.
+    nodo = nodo_con_costo_minore(costoNodi)
 
 
-def rielaboraPercorso():    # Funzione per rielaborare l'array parents e restituire il percorso effettivo.
+def rielaboraPercorso():  # Funzione per rielaborare l'array parents e restituire il percorso effettivo.
     val = reversed(parents.values())
     tmp = None
     out = []
@@ -106,8 +105,6 @@ def stampaPercorso(route):  # Da in out il percorso vero e proprio sottoforma di
     routeF += getLastNode()
     return routeF.upper()
 
+
 # TODO: Verificare che l'output funzioni su altri esempi di algoritmi.
 print("ROUTE: ", stampaPercorso(rielaboraPercorso()))
-
-
-

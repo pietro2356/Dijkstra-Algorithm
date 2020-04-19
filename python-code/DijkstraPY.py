3c92b8ff9d0d89c984eb704eb0e56b11f4faf521
pietro.rocchio <pietro.rocchio@marconirovereto.it>
2020-04-19 17:47:23 +0200

    Implementato il grafo di partenza.

---
+++ python-code/DijkstraPY.py
@@ -0,0 +1,14 @@
+# Implementazione del grafo
+grafo = {}
+grafo["start"] = {}
+grafo["start"]["a"] = 6
+grafo["start"]["b"] = 2
+
+grafo["a"] = {}
+grafo["a"]["end"] = 1
+
+grafo["b"] = {}
+grafo["b"]["a"] = 3
+grafo["b"]["end"] = 5
+
+grafo["end"] = {}

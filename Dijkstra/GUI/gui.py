1481ee0fa3a9fb869d78ab4c0ded9ff4f924be30
pietro.rocchio <pietro.rocchio@marconirovereto.it>
2020-05-01 20:22:33 +0200

    Implementato il funzionamento base per aggiungere nodi.

--- Dijkstra/GUI/gui.py
+++ Dijkstra/GUI/gui.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import *
+from Dijkstra.Algorithm.Interface import Interface
 
 # Configurazione gui
 win = tk.Tk()
@@ -11,8 +13,23 @@ win.configure(background="#eee")
 # ---------------------------------------------
 # Funzioni per interazione
 def addNodo():
-    pass
-    # Inserirli nel ListBox
+    if txtInNomeNodo.get():
+        nomeNodo = txtInNomeNodo.get()
+        lstNodi.insert("end", nomeNodo)
+        txtInNomeNodo.delete(0, "end")
+
+        Interface.createNode(nomeNodo)  # Creazione del nodo.
+    else:
+        tk.messagebox.showinfo("Avviso", "Il valore inserito non è valido!")
+
+
+def onselect(evt):
+    w = evt.widget
+    index = int(w.curselection()[0])
+    value = w.get(index)
+    print('Item selezionato => { %d : "%s" } ' % (index, value))
+    lblSelNodo = tk.Label(win, text=("Nodo selezionato: %s" % value), font=("arial", 12))
+    lblSelNodo.grid(row=1, column=1, pady=20, sticky="N")
 
 
 # ---------------------------------------------
@@ -32,9 +49,11 @@ btnAddNodo.grid(row=0, column=2, padx=10, sticky="W")
 
 # Row 1
 # ListBox Nodi
-lstNodi = tk.Listbox(win, height=15, width=25)
+lstNodi = tk.Listbox(win, height=15, width=25, selectmode=EXTENDED)
 lstNodi.grid(row=1, column=0, pady=20, sticky="W")
+lstNodi.bind('<<ListboxSelect>>', onselect)
 
 
+# Funzione di avvio
 if __name__ == "__main__":
     win.mainloop()

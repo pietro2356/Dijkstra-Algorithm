ca0f7c3f49bd35089c87df8c50bbbffc00d3052c
pietro.rocchio <pietro.rocchio@marconirovereto.it>
2020-05-01 19:27:19 +0200

    Inizializzata la GUI.

---
+++ Dijkstra/GUI/gui.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+# Configurazione gui
+win = tk.Tk()
+win.geometry("500x400")
+win.title("Dijkstra Algorithm GUI")
+win.resizable(False, False)
+win.configure(background="#eee")
+
+
+
+
+if __name__ == "__main__":
+    win.mainloop()
